98d0f7d44fce5ae1a9bbaf6e5297caa73218aac6
utesurdako <arktnels@gmail.com>
2021-09-29 19:52:14 +0300

    Удалены логи

--- Assets/MyData/Scripts/TestScropt.cs
+++ Assets/MyData/Scripts/TestScropt.cs
@@ -7,9 +7,7 @@ public class TestScropt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Log");
-        Debug.LogWarning("Warning");
-        Debug.LogError("Error");
+
     }
 
     // Update is called once per frame
